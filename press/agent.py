aaaa54c937f054bf11ae79cd45d5f09ea2f46b8d
Abdelrhaman Saeed <118386543+abdosaeed95@users.noreply.github.com>
2025-08-13 18:19:25 +0300

    fix: remove agent request fialure

--- press/agent.py
+++ press/agent.py
@@ -924,7 +924,7 @@ class Agent:
 			)
 
 	def raise_if_past_requests_have_failed(self):
-		failures = frappe.db.get_value("Agent Request Failure", {"server": self.server}, "failure_count")
+		failures = None#frappe.db.get_value("Agent Request Failure", {"server": self.server}, "failure_count")
 		if failures:
 			raise AgentRequestSkippedException(f"Previous {failures} requests have failed. Try again later.")
 
