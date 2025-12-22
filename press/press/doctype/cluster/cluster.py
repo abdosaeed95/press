bead67a386359e13233775ee6415341896c11fb5
abdo.saeed95 <abdo@brandimic.com>
2025-12-22 20:06:27 +0200

    fix: proxy sql port

--- press/press/doctype/cluster/cluster.py
+++ press/press/doctype/cluster/cluster.py
@@ -466,10 +466,10 @@ class Cluster(Document):
 					"ToPort": 2222,
 				},
 				{
-					"FromPort": 3307,
+					"FromPort": 3306,
 					"IpProtocol": "tcp",
 					"IpRanges": [{"CidrIp": "0.0.0.0/0", "Description": "ProxySQL from anywhere"}],
-					"ToPort": 3307,
+					"ToPort": 3309,
 				},
 			],
 		)
@@ -616,7 +616,7 @@ class Cluster(Document):
 						direction="INGRESS",
 						protocol="6",
 						source="0.0.0.0/0",
-						tcp_options=TcpOptions(destination_port_range=PortRange(min=3307, max=3307)),
+						tcp_options=TcpOptions(destination_port_range=PortRange(min=3309, max=3309)),
 					),
 					AddSecurityRuleDetails(
 						description="Everything to anywhere",
