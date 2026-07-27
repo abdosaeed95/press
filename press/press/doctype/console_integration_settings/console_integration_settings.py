4a046a8fe939363846e7943fb573f7b7b9440de8
abdosaeed95 <abdelrahman@divic.tech>
2026-07-27 15:08:32 +0300

    feat(updates): auto-select no-slave sites (FA-2392)

---
+++ press/press/doctype/console_integration_settings/console_integration_settings.py
@@ -0,0 +1,61 @@
+# Copyright (c) 2026, Frappe and contributors
+# For license information, please see license.txt
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import frappe
+from frappe import _
+from frappe.frappeclient import FrappeClient
+from frappe.model.document import Document
+
+if TYPE_CHECKING:
+	from frappe.types import DF
+
+
+class ConsoleIntegrationSettings(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	if TYPE_CHECKING:
+		console_url: DF.Data
+		password: DF.Password
+		username: DF.Data
+	# end: auto-generated types
+
+	def get_sites_without_slaves(self, sites: list[str]) -> list[str]:
+		password = self.get_password("password", raise_exception=False)
+		if not (self.console_url and self.username and password):
+			frappe.throw(_("Configure Console Integration Settings before selecting sites for update."))
+
+		with FrappeClient(self.console_url.rstrip("/"), self.username, password) as client:
+			masters = client.get_list(
+				"Instance",
+				fields=["name", "subscription"],
+				filters={
+					"name": ["in", sites],
+					"instance_type": "Master",
+					"status": ["!=", "Archived"],
+				},
+				limit_page_length=len(sites),
+			)
+			subscriptions = [master["subscription"] for master in masters if master.get("subscription")]
+			slaves = (
+				client.get_list(
+					"Instance",
+					fields=["subscription"],
+					filters={
+						"subscription": ["in", subscriptions],
+						"instance_type": "Slave",
+						"status": ["!=", "Archived"],
+					},
+					limit_page_length=10000,
+				)
+				if subscriptions
+				else []
+			)
+
+		slave_subscriptions = {slave["subscription"] for slave in slaves}
+		master_subscriptions = {master["name"]: master.get("subscription") for master in masters}
+		return [site for site in sites if master_subscriptions.get(site) not in slave_subscriptions]
